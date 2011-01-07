532a86db26c5bbf5f268277f6fa4de33a402dc8a
Dion Almaer <dion@almaer.com>
2011-01-07 15:00:16 -0800

    DB not Couch and other small tweety things

---
+++ server/data/sample.js
@@ -0,0 +1,22 @@
+// Tweet
+{
+    "from_user_id_str": "1383740",
+    "profile_image_url": "http://a3.twimg.com/profile_images/58891363/headshot-6_normal.png",
+    "created_at": "Sat, 13 Nov 2010 20:51:40 +0000",
+    "from_user": "bgalbs",
+    "id_str": "3550584942628864",
+    "metadata": {
+        "result_type": "recent"
+    },
+    "to_user_id": null,
+    "text": "What's up with iTunes' lack of movie selection? Hardly any of the greats from 2009. Is Amazon's on-line store any better?",
+    "id": "3550584942628864",
+    "from_user_id": "1383740",
+    "geo": null,
+    "iso_language_code": "en",
+    "to_user_id_str": null,
+    "source": "&lt;a href=&quot;http://www.echofon.com/&quot; rel=&quot;nofollow&quot;&gt;Echofon&lt;/a&gt;",
+    "type": "tweet"
+}
+
+// Blog
--- server/tweet-importer.js
+++ server/tweet-importer.js
@@ -1,10 +1,44 @@
 var sys = require('sys'),
-    yql = require('yql');
+    yql = require('yql'),
+    request = require('request'),
+    h = {accept:'application/json', 'content-type':'application/json'},
+    DB_URL = "http://localhost:5984/setdirection_blog/",
+    POSTS_VIEW_URL = DB_URL + "_design/posts/_view/";
 
-new yql.exec("select * from twitter.search where q in ('from:dalmaer', 'from:bgalbs') | sort(field='created_at',descending='true')", function(response) {
-    sys.puts(JSON.stringify(response.query.results));
-    sys.puts("\n\n");
+///blog/_design/docs/_view/by_date?key="2009/01/30 18:04:11"
+//http://localhost:5984/setdirection_blog/_design/posts/_view/by_id?key=%22set-direction-launches%22
+
+
+new yql.exec("select * from twitter.search where q in ('from:dalmaer', 'from:bgalbs') | sort(field='created_at',descending='true')", function (response) {
+    response.query.results.results.forEach(function(tweet) {
+        if (tweetIsNotAlreadyInDb(tweet)) {
+            tweet.type = "tweet"; // tag is as a tweet
+            storeTweet(tweet);
+        }
+        sys.puts(JSON.stringify(tweet));
+        sys.puts(url_for_tweet_id(tweet.id));
+    });
 });
+
+var tweetIsNotAlreadyInDb = function(tweet) {
+    request({uri:url_for_tweet_id(tweet.id) + '"', headers:h}, function (err, response, body) {
+      sys.puts(sys.inspect(JSON.parse(body)));
+    });
+
+    return true;
+}
+
+var storeTweet = function(tweet) {
+    sys.puts("storing tweet");
+}
+
+var url_for_tweet_id = function(key) {
+    return POSTS_VIEW_URL + "by_tweet_id" + ((key !== undefined) ? '?key="' + key + '"': "");
+}
+
+var url_for_blog_id = function(key) {
+    return POSTS_VIEW_URL + "by_blog_id" + ((key !== undefined) ? '?key="' + key + '"': "");
+}
 
 // 
 // var tweetstream = require('tweetstream'),
