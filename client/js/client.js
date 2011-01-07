587470fb824ec4fca8225a8099b3247685d73ace
Dion Almaer <dion@almaer.com>
2011-01-07 15:16:30 -0800

    put failure case and unified naming

--- client/js/client.js
+++ client/js/client.js
@@ -1,11 +1,31 @@
 var client = (function() {
     
+    // -- Settings and configuration
+    
     // default settings values; can be overridden by the user
     var settings = {
         storiesOnHome: 10,
         showFullStory: false,
         contentBlockId: "content"
     }
+
+    // -- Private methods
+    
+    // called when the request to retrieve the stories has been successful and blows away the existing content
+    // block contents to display them
+    var displayStoriesSuccess = function(data) {
+        // will automatically hide the main stories loading indicator if shown
+        ui.addStoriesToMain(data);
+    }
+
+    // used when the story retrieval fails and there is no content for the user to see. this is really bad
+    var displayStoriesFailure = function(errorCode) {
+        console.log("on error");
+
+        ui.newMainStoriesNotLoaded(errorCode);
+    }
+
+    // -- The Public API
 
     return {
         // save the settings
@@ -18,27 +38,17 @@ var client = (function() {
         // loading indicator
         displayStories: function() {
             // check for the blog object; if not found, throw an error
-            if (!blog) throw { message: "Blog instance not created", type: ERROR_CODES.INTERNAL_STATE_INVALID };
+            if (!blog) throw { message: "Blog instance not created", type: ERROR.INTERNAL_STATE_INVALID };
             
             // display a loading indicator
             ui.showMainStoriesLoading();
 
             // requests the posts and pass in a callback
-            blog.posts({ count: settings.storiesOnHome, onSuccess: client.displayStoriesSuccess });
-        },
-        
-        // called when the request to retrieve the stories has been successful and blows away the existing content
-        // block contents to display them
-        displayStoriesSuccess: function(data) {
-            // will automatically hide the main stories loading indicator if shown
-            ui.addStoriesToMain(data);
-        },
-
-        // used when the story retrieval fails and there is no content for the user to see. this is really bad
-        displayStoriesError: function(errorCode) {
-            console.log("on error");
-
-            ui.newMainStoriesNotLoaded(errorCode);
-        }
+            blog.posts({
+                count: settings.storiesOnHome,
+                onSuccess: displayStoriesSuccess,
+                onFailure: displayStoriesFailure
+            });
+        }        
     }
 })();
--- client/js/errorCodes.js
+++ client/js/errorCodes.js
@@ -3,6 +3,6 @@
 //          (e.g., a required variable is missing, data is corrupt)
 //      codes > 0 are errors that the user can do something about
 //          (e.g., the browser is unsupported, inappropriate word in a comment)
-var ERROR_CODES = {
+var ERROR = {
     INTERNAL_STATE_INVALID: -1     // generic error that shouldn't happen but did (state corruption issues, etc.)
 }
